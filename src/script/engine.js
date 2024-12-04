745ac7518e04936cc9811b8d86d3d49efcf53a75
hilario <hcraftt@gmail.com>
2024-12-03 23:58:46 -0300

    projeto iniciado

---
+++ src/script/engine.js
@@ -0,0 +1,39 @@
+const emojis = [
+    "🤪",
+    "🤪",
+    "🥶",
+    "🥶",
+    "🥵",
+    "🥵",
+    "😡",
+    "😡",
+    "🤮",
+    "🤮",
+    "🤡",
+    "🤡"
+];
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() =>
+    (Math.random() > 0.5 ? 2 : -1))
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
+
+}
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classlist.add("boxOpen");
+        openCards.push(this);
+    }
+    if (openCards.length == 2) {
+        setTimeout(checkMath, 500);
+    }
+}
+function checkMath() {
+
+}
